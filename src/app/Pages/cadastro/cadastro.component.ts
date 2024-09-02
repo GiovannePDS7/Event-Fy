ec3b1b30eb1e6cc36f12495b26c3005175d4c41f
Giovanne Pagano <giovanne3282@gmail.com>
2024-09-02 18:32:20 -0300

    desenvolvendo cadastro

--- src/app/Pages/cadastro/cadastro.component.ts
+++ src/app/Pages/cadastro/cadastro.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  styleUrls: ['./cadastro.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CadastroComponent {
 
