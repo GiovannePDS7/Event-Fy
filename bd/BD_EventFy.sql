6d5dc5b199a67d528a98604dba8919d35b76483d
Anderson-Armando <arevalosanderson9@gmail.com>
2024-09-02 18:51:29 -0300

    Inserção de tabelas baseadas no DER no Banco de Dados

--- bd/BD_EventFy.sql
+++ bd/BD_EventFy.sql
@@ -3,49 +3,67 @@ CREATE DATABASE EventFy;
 USE EventFy;
 
 CREATE TABLE Organizador(
-	Id_Organizador		INT PRIMARY KEY IDENTITY(1, 1),
-	Nome_Organizador	CHAR(60) NOT NULL,
-	Email_Organizador	VARCHAR(50) NOT NULL,
-	Senha_Oganizador	VARCHAR(50) NOT NULL,
+	Id_Organizador		  INT PRIMARY KEY IDENTITY(1, 1),
+	Nome_Organizador	  CHAR(60) NOT NULL,
+	Email_Organizador	  VARCHAR(50) NOT NULL,
+	Senha_Oganizador	  VARCHAR(50) NOT NULL,
 	Contato_Organizador	VARCHAR(20) NOT NULL,
-	Data_Cadastro		DATETIME
+	Data_Cadastro		    DATETIME
 )
 
 CREATE TABLE Evento(
-	Id_Evento			INT PRIMARY KEY IDENTITY(1, 1),
-	Nome_Evento			VARCHAR(40) UNIQUE NOT NULL,
-	Data_Evento			DATE UNIQUE NOT NULL,
-	Local_Evento		VARCHAR(50) NOT NULL,
-	Horario_Evento		TIME NOT NULL
+	Id_Evento			      INT PRIMARY KEY IDENTITY(1, 1),
+	Nome_Evento			    VARCHAR(40) UNIQUE NOT NULL,
+	Data_Evento			    DATE UNIQUE NOT NULL,
+	Local_Evento		    VARCHAR(50) NOT NULL,
+	Horario_Evento		  TIME NOT NULL
 )
 
 CREATE TABLE Agenda(
-	Id_Agenda			INT PRIMARY KEY IDENTITY(1, 1),
-	Calendario_Agenda	DATE UNIQUE,
-	Horario_Agenda		TIME UNIQUE,
-	Descricao_Agenda	VARCHAR(70)
+	Id_Agenda			      INT PRIMARY KEY IDENTITY(1, 1),
+	Calendario_Agenda	  DATE UNIQUE,
+	Horario_Agenda		  TIME UNIQUE,
+	Descricao_Agenda	  VARCHAR(70)
 )
 
 CREATE TABLE Feedback(
-	Id_Feedback			INT PRIMARY KEY IDENTITY(1, 1),
-	Txt_Feedback		VARCHAR(200)
-)
-
-CREATE TABLE Relatorio(
-	Id_Relatorio		INT PRIMARY KEY IDENTITY(1, 1),
-	Nome_Relatorio		VARCHAR(35) NOT NULL,
-	Tipo_Relatorio		CHAR(30) NOT NULL,
-	Descricao_Relatorio	VARCHAR(500),
-	Id_Evento			INT,
+	Id_Feedback			    INT PRIMARY KEY IDENTITY(1, 1),
+	Txt_Feedback		    VARCHAR(200) NOT NULL,
+  Id_Evento			      INT,
 	CONSTRAINT Fk_Id_Evento FOREIGN KEY (Id_Evento) REFERENCES Evento(Id_Evento) ON DELETE CASCADE
 )
 
+CREATE TABLE Ingresso(
+	Id_Ingresso         INT PRIMARY KEY IDENTITY(1, 1),
+  Nome_Ingresso       CHAR(60) NOT NULL,
+  CPF_Ingresso        VARCHAR(15) NOT NULL,
+  Tipo_Ingresso       VARCHAR(30) NOT NULL,
+  Data_Compra         DATE NOT NULL,
+  Data_Evento         DATE UNIQUE NOT NULL,
+  CONSTRAINT Fk_Data_Evento FOREIGN KEY (Data_Evento) REFERENCES Evento(Data_Evento) ON DELETE CASCADE
+)
+
 CREATE TABLE Perfil(
-	Id_Perfil			INT PRIMARY KEY IDENTITY(1, 1),
-	Tipo_Perfil			VARCHAR(30),
-	Nome_Perfil			CHAR(50),
-	Contato_Perfil		VARCHAR(20),
-	Email_Perfil		VARCHAR(50)
+	Id_Perfil			      INT PRIMARY KEY IDENTITY(1, 1),
+	Tipo_Perfil			    VARCHAR(30) NOT NULL,
+	Nome_Perfil			    CHAR(50) NOT NULL,
+	Contato_Perfil		  VARCHAR(20) NOT NULL,
+	Email_Perfil		    VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Colaborador(
+  Id_Colaborador      INT PRIMARY KEY IDENTITY(1, 1),
+  Tipo_Colaborador    VARCHAR(70) NOT NULL,
+  Funcao_Colaborador  VARCHAR(100) NOT NULL,
+  Nome_Colaborador    CHAR(50) NOT NULL, 
+)
+
+CREATE TABLE Cliente(
+  Id_Cliente          INT PRIMARY KEY IDENTITY(1, 1),
+  Nome_Cliente        CHAR(60) NOT NULL,
+  Data_Nasc_Cliente   DATE NOT NULL,
+  Email_Cliente       VARCHAR(50) NOT NULL,
+  Contato_Cliente     VARCHAR(20) NOT NULL
 )
 
 
