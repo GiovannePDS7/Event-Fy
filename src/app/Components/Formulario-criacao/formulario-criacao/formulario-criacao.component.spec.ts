ae89fda3c75fc059672dfcad4383b9205e493544
Pedro do Piauí <pedrosoares383@gmail.com>
2024-10-29 12:27:39 -0300

    criacao formulario desktop part1

--- src/app/Components/Formulario-criacao/formulario-criacao/formulario-criacao.component.spec.ts
+++ src/app/Components/Formulario-criacao/formulario-criacao/formulario-criacao.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormularioCriacaoComponent } from './formulario-criacao.component';
 
+
+
 describe('FormularioCriacaoComponent', () => {
   let component: FormularioCriacaoComponent;
   let fixture: ComponentFixture<FormularioCriacaoComponent>;
