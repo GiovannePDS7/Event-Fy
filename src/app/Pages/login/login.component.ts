7bf195cb41df0117e4ff320d14b1572b41215c83
giovanne <giovanne3282@gmail.com>
2024-11-04 11:17:59 -0300

    desenvolvendo login

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { catchError, map, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { LoginResponseDTO } from 'src/app/models/login-response-dto';
 
 interface EmailCheckResponse {  // Definindo a interface aqui
@@ -46,58 +46,68 @@ export class LoginComponent {
   visibilidadeSenha(): void {
     this.typeSenha = this.typeSenha === 'password' ? 'text' : 'password';
   }
-  verificarEmail() {
+  verificarEmail(): Observable<boolean> {
     const email = this.LoginForm.get('emailOrganizador')?.value;
 
     if (email) {
-      this.http.get<EmailCheckResponse>(`${this.apiUrl}/verificar-email?email=${email}`).pipe(
-        catchError(() => of({ existe: false }))
-      ).subscribe((resposta: EmailCheckResponse) => {
-        this.emailExiste = resposta.existe;
-        if (this.emailExiste) {
-          this.LoginForm.get('emailOrganizador')?.setErrors({ emailJaExiste: true });
-          console.log("Email verificado no banco");
-        }
-      });
+      return this.http.get<EmailCheckResponse>(`${this.apiUrl}/verificar-email?email=${email}`).pipe(
+        map((resposta: EmailCheckResponse) => {
+          this.emailExiste = resposta.existe;
+          if (this.emailExiste) {
+            this.LoginForm.get('emailOrganizador')?.setErrors({ emailJaExiste: true });
+            console.log("Email verificado no banco");
+          }
+          return this.emailExiste;
+        }),
+        catchError(() => {
+          this.emailExiste = false;
+          return of(false);
+        })
+      );
+    } else {
+      this.emailExiste = false;
+      return of(false);
     }
   }
-
 
   onEnviar() {
     this.LoginForm.markAllAsTouched();
-    this.verificarEmail();
-    console.log('entrou 1')
-    console.log(this.LoginForm); // Exibe os erros do formulário
-    console.log(this.emailExiste);
 
-    if (this.LoginForm.valid && this.emailExiste) {
-      const { emailOrganizador, senhaLogin } = this.LoginForm.value;
-      console.log('entrou 2')
-      this.http.post<LoginResponseDTO>(`${this.apiUrl}/login`, {
-        emailOrganizador: emailOrganizador,
-        senhaOrganizador: senhaLogin
-      }).subscribe({
-        next: (response: LoginResponseDTO) => {
-          localStorage.setItem('token', response.token);
-          localStorage.setItem('organizadorId', response.id.toString());
-          localStorage.setItem('nomeOrganizador', response.nome);
-          localStorage.setItem('emailOrganizador', response.email);
-          localStorage.setItem('contatoOrganizador', response.contato);
-          localStorage.setItem('fotoOrganizador', response.foto);
+    this.verificarEmail().subscribe((existe: any) => {
+      console.log('entrou 1');
+      console.log(this.LoginForm); // Exibe os erros do formulário
+      console.log(existe);
 
-          this.router.navigate(['/inicial']);
-        },
-        error: (err: HttpErrorResponse) => {
-          console.error('Erro de autenticação', err);
-          if (err.error && err.error.message) {
-            alert(`Erro: ${err.error.message}`);
-          } else {
-            alert('Erro desconhecido, tente novamente.');
+      if (this.LoginForm.valid && existe) {
+        const { emailOrganizador, senhaLogin } = this.LoginForm.value;
+        console.log('entrou 2');
+        this.http.post<LoginResponseDTO>(`${this.apiUrl}/login`, {
+          emailOrganizador: emailOrganizador,
+          senhaOrganizador: senhaLogin
+        }).subscribe({
+          next: (response: LoginResponseDTO) => {
+            localStorage.setItem('token', response.token);
+            localStorage.setItem('organizadorId', response.id.toString());
+            localStorage.setItem('nomeOrganizador', response.nome);
+            localStorage.setItem('emailOrganizador', response.email);
+            localStorage.setItem('contatoOrganizador', response.contato);
+            localStorage.setItem('fotoOrganizador', response.foto);
+
+            this.router.navigate(['/inicial']);
+          },
+          error: (err: HttpErrorResponse) => {
+            console.error('Erro de autenticação', err);
+            if (err.error && err.error.message) {
+              alert(`Erro: ${err.error.message}`);
+            } else {
+              alert('Erro desconhecido, tente novamente.');
+            }
           }
-        }
-      });
-    } else {
-      console.warn('formulário inválido.');
-    }
+        });
+      } else {
+        console.warn('formulário inválido.');
+      }
+    });
   }
+
 }
