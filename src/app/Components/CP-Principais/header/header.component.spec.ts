3823b41a56e29dca1ce6ea46ad95cc8900b11c38
JoaoEGiovanne <joaogiovanneetec@gmail.com>
2024-11-30 15:31:44 -0300

    menu hamburguer

--- src/app/Components/CP-Principais/header/header.component.spec.ts
+++ src/app/Components/CP-Principais/header/header.component.spec.ts
@@ -18,4 +18,54 @@ describe('HeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should start with the sidebar closed', () => {
+    // Certifique-se de que o menu lateral está fechado ao iniciar
+    expect(component.isSidebarActive).toBeFalse();
+  });
+
+  it('should open sidebar when toggleSidebar is called', () => {
+    // Chamar toggleSidebar para abrir o menu
+    component.toggleSidebar();
+    expect(component.isSidebarActive).toBeTrue();
+  });
+
+  it('should close sidebar when toggleSidebar is called again', () => {
+    // Abrir o menu primeiro
+    component.toggleSidebar();
+    expect(component.isSidebarActive).toBeTrue();
+
+    // Fechar o menu chamando toggleSidebar novamente
+    component.toggleSidebar();
+    expect(component.isSidebarActive).toBeFalse();
+  });
+
+  it('should close sidebar when closeSidebar is called', () => {
+    // Forçar o estado ativo do menu
+    component.isSidebarActive = true;
+    expect(component.isSidebarActive).toBeTrue();
+
+    // Chamar closeSidebar para fechar o menu
+    component.closeSidebar();
+    expect(component.isSidebarActive).toBeFalse();
+  });
+
+  it('should only open sidebar when clicking the icon', () => {
+    // Simular o clique no ícone chamando toggleSidebar
+    component.toggleSidebar();
+    expect(component.isSidebarActive).toBeTrue();
+
+    // Chamar closeSidebar e verificar que o menu fecha
+    component.closeSidebar();
+    expect(component.isSidebarActive).toBeFalse();
+  });
+  it('should close sidebar when X button is clicked', () => {
+    // Simula que o menu está aberto
+    component.isSidebarActive = true;
+    expect(component.isSidebarActive).toBeTrue();
+  
+    // Simula o clique no botão "X" (chamando toggleSidebar)
+    component.toggleSidebar();
+    expect(component.isSidebarActive).toBeFalse();
+  });
 });
--- src/app/Components/CP-Principais/header/header.component.ts
+++ src/app/Components/CP-Principais/header/header.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isSidebarActive = false;
 
+  toggleSidebar(): void {
+    this.isSidebarActive = !this.isSidebarActive;
+  }
+
+  closeSidebar(): void {
+    this.isSidebarActive = false;
+  }
 }
