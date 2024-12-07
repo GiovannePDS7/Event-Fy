384043acf40ccc02d5e890122b7546b11453ef15
Anderson-Armando <arevalosanderson9@gmail.com>
2024-12-07 14:15:29 -0300

    Tela de Esqueceu Senha

--- src/app/Pages/esqueceu-senha/esqueceu-senha.component.ts
+++ src/app/Pages/esqueceu-senha/esqueceu-senha.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./esqueceu-senha.component.css']
 })
 export class EsqueceuSenhaComponent {
+  email: string = '';
 
+  onSubmit() {
+    if (this.email) {
+      // Implemente a lógica de envio aqui, como chamar um serviço
+      console.log('E-mail enviado para:', this.email);
+    } else {
+      console.log('Por favor, insira um e-mail válido.');
+    }
+  }
 }
